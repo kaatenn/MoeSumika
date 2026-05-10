06e7f6a81d2d08cd58664907f6194f7d68ecc72c
kaatenn <komeijikaatenn@gmail.com>
2026-05-10 17:57:00 +0800

    init project

---
+++ MoeSumika/MoeSumikaCode/Potions/MoeSumikaPotion.cs
@@ -0,0 +1,8 @@
+﻿using BaseLib.Abstracts;
+using BaseLib.Utils;
+using MoeSumika.MoeSumikaCode.Character;
+
+namespace MoeSumika.MoeSumikaCode.Potions;
+
+[Pool(typeof(MoeSumikaPotionPool))]
+public abstract class MoeSumikaPotion : CustomPotionModel;
---
+++ MoeSumika/MoeSumikaCode/Powers/MoeSumikaPower.cs
@@ -0,0 +1,13 @@
+﻿using BaseLib.Abstracts;
+using BaseLib.Extensions;
+using MoeSumika.MoeSumikaCode.Extensions;
+using Godot;
+
+namespace MoeSumika.MoeSumikaCode.Powers;
+
+public abstract class MoeSumikaPower : CustomPowerModel
+{
+    //Loads from MoeSumika/images/powers/your_power.png
+    public override string CustomPackedIconPath => $"{Id.Entry.RemovePrefix().ToLowerInvariant()}.png".PowerImagePath();
+    public override string CustomBigIconPath => $"{Id.Entry.RemovePrefix().ToLowerInvariant()}.png".BigPowerImagePath();
+}
